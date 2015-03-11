ba36dde3a0118e360f88e236f50504fe2645d989
newagebegins <newagebegins@gmail.com>
2015-03-11 21:21:07 +1000

    init

---
+++ init.el
@@ -0,0 +1,14 @@
+;; Packages that I use:
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq-default fill-column 80)
+(prefer-coding-system 'utf-8)
+(tool-bar-mode 0)
+(show-paren-mode)
+(setq default-input-method "russian-computer")
+(setq-default indicate-empty-lines t)
+(setq ring-bell-function 'ignore)
