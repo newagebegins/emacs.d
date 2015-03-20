3222a7aee23b616bd56cf67bc1a634fd9f864ee9
newagebegins <newagebegins@gmail.com>
2015-03-20 10:10:48 +1000

    configure style for C

--- init.el
+++ init.el
@@ -9,8 +9,17 @@
 (setq-default fill-column 80)
 (prefer-coding-system 'utf-8)
 (tool-bar-mode 0)
+(blink-cursor-mode 0)
 (show-paren-mode)
 (setq default-input-method "russian-computer")
+;;(global-set-key (kbd "M-l") 'toggle-input-method)
 (setq-default indicate-empty-lines t)
 (setq ring-bell-function 'ignore)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+(setq-default indent-tabs-mode nil)
+
+(setq-default c-basic-offset 4)
+(setq c-default-style "stroustrup")
+(add-hook 'c-mode-common-hook
+          (lambda ()
+	    (c-set-offset 'case-label '+)))
