4d8877d382e7fefb8293a9620515824bc8538cc7
newagebegins <newagebegins@gmail.com>
2015-04-12 15:41:04 +1000

    tweak language switching code

--- init.el
+++ init.el
@@ -34,8 +34,8 @@
 
 (prefer-coding-system 'utf-8)
 (setq default-input-method "russian-computer")
-(global-set-key (kbd "M-o") 'toggle-input-method)
-(global-set-key (kbd "M-o") 'isearch-toggle-input-method)
+(global-set-key (kbd "M-l") 'toggle-input-method)
+(define-key isearch-mode-map (kbd "M-l") 'isearch-toggle-input-method)
 
 ;; Disable annoying sounds on Windows.
 (setq ring-bell-function 'ignore)
