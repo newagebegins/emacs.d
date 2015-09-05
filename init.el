757841e2fa386ab5af05240dd59c59fcaefae76a
newagebegins <newagebegins@gmail.com>
2015-09-06 08:23:59 +1000

    Add 'my-keys' minor mode.

--- init.el
+++ init.el
@@ -7,20 +7,20 @@
                     scss-mode
                     web-mode
                     zenburn-theme
-                    yaml-mode))
+                    yaml-mode
+                    monokai-theme))
 
-;; Fetch the list of available packages.
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Install the missing packages.
+;; Install missing packages.
 (dolist (package my-packages)
   (unless (package-installed-p package)
     (package-install package)))
 
 ;; Load Zenburn theme.
-(setq custom-safe-themes '("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))
-(load-theme 'zenburn t)
+;;(setq custom-safe-themes '("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))
+;;(load-theme 'zenburn t)
+
+(setq custom-safe-themes '("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default))
+(load-theme 'monokai t)
 
 ;; Tweak font size.
 ;;(set-face-attribute 'default nil :height 98)
@@ -43,11 +43,11 @@
 (menu-bar-mode -1)
 (tool-bar-mode 0)
 (blink-cursor-mode 0)
-(global-hl-line-mode 1)
+;;(global-hl-line-mode 1)
 (scroll-bar-mode -1)
 (setq inhibit-startup-screen t)
 ;; Smooth scrolling.
-(setq scroll-step 3)
+;;(setq scroll-step 3)
 ;; Use Github Flavored Markdown mode for markdown files.
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -100,18 +100,22 @@
 (require 'ido)
 (ido-mode t)
 
-(global-set-key [f10] 'recompile)
-(global-set-key [f11] 'previous-error)
-(global-set-key [f12] 'next-error)
-(global-set-key (kbd "M-r") 'save-buffer)
-(global-set-key (kbd "M-v") 'switch-to-buffer)
-(global-set-key (kbd "M-V") 'switch-to-buffer-other-window)
-(global-set-key (kbd "M-k") 'kill-this-buffer)
-(global-set-key (kbd "C-t") 'other-window)
-(global-set-key (kbd "M-f") 'find-file)
-(global-set-key (kbd "M-F") 'find-file-other-window)
-(global-set-key (kbd "M-1") 'delete-other-windows)
-(global-set-key (kbd "C-e") 'kill-ring-save)
-(global-set-key (kbd "C-v") 'yank)
-(global-set-key (kbd "C-d") 'kill-line)
-(global-set-key (kbd "M-o") (kbd "C-u C-SPC"))
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+(define-key my-keys-minor-mode-map [f10] 'recompile)
+(define-key my-keys-minor-mode-map [f11] 'previous-error)
+(define-key my-keys-minor-mode-map [f12] 'next-error)
+(define-key my-keys-minor-mode-map (kbd "M-r") 'save-buffer)
+(define-key my-keys-minor-mode-map (kbd "M-v") 'switch-to-buffer)
+(define-key my-keys-minor-mode-map (kbd "M-V") 'switch-to-buffer-other-window)
+(define-key my-keys-minor-mode-map (kbd "M-k") 'kill-this-buffer)
+(define-key my-keys-minor-mode-map (kbd "C-t") 'other-window)
+(define-key my-keys-minor-mode-map (kbd "M-f") 'find-file)
+(define-key my-keys-minor-mode-map (kbd "M-F") 'find-file-other-window)
+(define-key my-keys-minor-mode-map (kbd "M-1") 'delete-other-windows)
+(define-key my-keys-minor-mode-map (kbd "C-e") 'kill-ring-save)
+(define-key my-keys-minor-mode-map (kbd "C-v") 'yank)
+(define-key my-keys-minor-mode-map (kbd "C-d") 'kill-line)
+(define-key my-keys-minor-mode-map (kbd "M-o") (kbd "C-u C-SPC"))
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
