764f3ac469ac94338b622f368e80aeab0d316b25
newagebegins <newagebegins@gmail.com>
2015-10-21 13:40:26 +1000

    Tweaks for Ruby.

--- init.el
+++ init.el
@@ -98,6 +98,7 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (setq coffee-tab-width 2)
+(setq ruby-insert-encoding-magic-comment nil)
 
 ;; =============================================================================
 ;; Helm, projectile, etc.
@@ -125,3 +126,15 @@
 (setq projectile-switch-project-action 'helm-projectile)
 
 (helm-descbinds-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
