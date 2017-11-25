dd6750c7adf718c855fa72954a431b3a34ec5c09
newagebegins <newagebegins@gmail.com>
2017-11-26 08:52:05 +1000

    add copy-file-name-to-clipboard

--- init.el
+++ init.el
@@ -169,6 +169,17 @@
   (interactive "r")
   (ansi-color-apply-on-region beg end))
 
+;; http://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 (defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
 
 (define-key my-keys-minor-mode-map [f10] 'recompile)
